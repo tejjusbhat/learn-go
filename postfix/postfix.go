e35e0a45340461f39f6ebda975450d3e9f5e0827
Tejjus Bhat <not.tejjusbhat@gmail.com>
2024-10-29 20:36:23 +0530

    Add initial implementation of postfix evaluation program

---
+++ postfix/postfix.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, playground")
+	// TODO: Create a program that evaluates postfix expressions
+}
---
+++ test/test.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "A"
+	fmt.Printf("%d", str[0])
+}
